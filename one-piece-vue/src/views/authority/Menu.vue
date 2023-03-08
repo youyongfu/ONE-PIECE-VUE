0f2bb5ef64413102610ab2240775aa73244d70fc
youyongfuLs <youyongfuLS@jomoo.cn>
2023-03-08 15:53:42 +0800

    菜单管理模块代码重构

--- one-piece-vue/src/views/authority/Menu.vue
+++ one-piece-vue/src/views/authority/Menu.vue
@@ -2,8 +2,9 @@
     <div>
 
         <el-form :inline="true">
+            <!-- 操作按钮-->
             <el-form-item>
-                <el-button type="primary" @click="dialogVisible = true" v-if="hasAuth('sys:menu:save')">新增</el-button>
+                <el-button type="primary"  @click="editHandle(null)" v-if="hasAuth('sys:menu:save')">新增</el-button>
             </el-form-item>
         </el-form>
 
@@ -59,116 +60,31 @@
                 :total="total">
         </el-pagination>
 
-        <!--新增对话框-->
-        <el-dialog title="提示" :visible.sync="dialogVisible" width="600px" :before-close="handleClose">
+        <MenuEdit title="编辑菜单" v-if="dialogVisible" ref="MenuEdit"/>
 
-            <el-form :model="editForm" :rules="editFormRules" ref="editForm" label-width="100px" class="demo-editForm">
-
-                <el-form-item label="上级菜单" prop="parentId">
-                    <el-select v-model="editForm.parentId" placeholder="请选择上级菜单">
-                        <template v-for="item in treeData">
-                            <el-option :label="item.name" :value="item.id" :key="item.name"></el-option>
-                            <template v-for="child in item.children">
-                                <el-option :label="child.name" :value="child.id" :key="child.name">
-                                    <span>{{ "- " + child.name }}</span>
-                                </el-option>
-                            </template>
-                        </template>
-                    </el-select>
-                    <el-alert title="未选择上级菜单，则默认为添加一级菜单" :closable="false" type="info" style="line-height: 12px;"></el-alert>
-                </el-form-item>
-
-                <el-form-item label="菜单名称" prop="name" label-width="100px">
-                    <el-input v-model="editForm.name" autocomplete="off"></el-input>
-                </el-form-item>
-
-                <el-form-item label="权限编码" prop="perms" label-width="100px">
-                    <el-input v-model="editForm.perms" autocomplete="off"></el-input>
-                </el-form-item>
-
-                <el-form-item label="图标" prop="icon" label-width="100px">
-                    <el-input v-model="editForm.icon" autocomplete="off"></el-input>
-                </el-form-item>
-
-                <el-form-item label="菜单URL" prop="path" label-width="100px">
-                    <el-input v-model="editForm.path" autocomplete="off"></el-input>
-                </el-form-item>
-
-                <el-form-item label="菜单组件" prop="component" label-width="100px">
-                    <el-input v-model="editForm.component" autocomplete="off"></el-input>
-                </el-form-item>
-
-                <el-form-item label="类型" prop="type" label-width="100px">
-                    <el-radio-group v-model="editForm.type">
-                        <el-radio :label=0>目录</el-radio>
-                        <el-radio :label=1>菜单</el-radio>
-                        <el-radio :label=2>按钮</el-radio>
-                    </el-radio-group>
-                </el-form-item>
-
-                <el-form-item label="状态" prop="statu" label-width="100px">
-                    <el-radio-group v-model="editForm.statu">
-                        <el-radio :label=0>禁用</el-radio>
-                        <el-radio :label=1>正常</el-radio>
-                    </el-radio-group>
-                </el-form-item>
-
-                <el-form-item label="排序号" prop="orderNum" label-width="100px">
-                    <el-input-number v-model="editForm.orderNum" :min="1" label="排序号">1</el-input-number>
-                </el-form-item>
-
-                <el-form-item>
-                    <el-button type="primary" @click="submitForm('editForm')">提交</el-button>
-                    <el-button @click="resetForm('editForm')">重置</el-button>
-                </el-form-item>
-            </el-form>
-
-        </el-dialog>
     </div>
 </template>
 
 <script>
+
+    import MenuEdit from "../authority/MenuEdit";
+
     export default {
         name: "Menu",
+        components:{MenuEdit},
         data() {
             return {
-                tableData:[],
-                treeData:[],
-                dialogVisible: false,
-                total: 0,
-                size: 10,
-                current: 1,
-                editForm: {},
-                editFormRules: {
-                    name: [
-                        {required: true, message: '请输入名称', trigger: 'blur'}
-                    ],
-                    perms: [
-                        {required: true, message: '请输入权限编码', trigger: 'blur'}
-                    ],
-                    type: [
-                        {required: true, message: '请选择状态', trigger: 'blur'}
-                    ],
-                    orderNum: [
-                        {required: true, message: '请填入排序号', trigger: 'blur'}
-                    ],
-                    statu: [
-                        {required: true, message: '请选择状态', trigger: 'blur'}
-                    ]
-                },
+                tableData:[],           //列表数据
+                total: 0,               //总条数
+                size: 10,               //每页显示条数
+                current: 1,             //页数
+                dialogVisible: false,   //是否显示编辑对话框
             }
         },
         created(){
             this.getListMenu()
-            this.getTreeMenu()
         },
         methods: {
-            //获取树形菜单
-            getTreeMenu(){
-                this.$axios.get('sys/menu/list').then(res =>{
-                    this.treeData = res.data.data;
-                })
-            },
             //获取菜单列表
             getListMenu(){
                 this.$axios.get('sys/menu/listPage',{params: {
@@ -199,48 +115,12 @@
                 this.current = val
                 this.getListMenu()
             },
-            //重置
-            resetForm(formName) {
-                this.$refs[formName].resetFields();
-                this.editForm = {}
-            },
-            //关闭对话框
-            handleClose() {
-                this.resetForm('editForm')
-                this.dialogVisible = false
-            },
-            //提交
-            submitForm(formName) {
-                this.$refs[formName].validate((valid) => {
-                    if (valid) {
-                        this.$axios.post('/sys/menu/' + (this.editForm.id?'update' : 'save'), this.editForm).then(res => {
-                            console.log(res)
-                            this.$message({
-                                showClose: true,
-                                message: '提交成功',
-                                type: 'success',
-                                onClose:() => {
-                                    this.getListMenu()
-                                }
-                            });
-                            this.resetForm('editForm')
-                            this.dialogVisible = false
-                        })
-                    } else {
-                        console.log('error submit!!');
-                        return false;
-                    }
-                });
-            },
             //编辑
             editHandle(id){
-                this.$axios.get('/sys/menu/info/' + id).then(res => {
-                    this.editForm = res.data.data;
-                    if(this.editForm.parentId === 0){
-                        this.editForm.parentId = "";
-                    }
-                })
-                this.dialogVisible = true
+                this.dialogVisible = true;
+                this.$nextTick(() => {
+                    this.$refs.MenuEdit.init(id);
+                });
             },
             //删除
             deleteHandle(id){
---
+++ one-piece-vue/src/views/authority/MenuEdit.vue
@@ -0,0 +1,179 @@
+<template>
+    <div>
+        <!-- 添加或修改业务对话框 -->
+        <el-dialog title="提示" :visible.sync="dialogVisible" width="600px" :before-close="handleClose">
+
+            <el-form :model="editForm" :rules="editFormRules" ref="editForm" label-width="100px" class="demo-editForm">
+
+                <el-form-item label="上级菜单" prop="parentId">
+                    <el-select v-model="editForm.parentId" placeholder="请选择上级菜单">
+                        <template v-for="item in treeData">
+                            <el-option :label="item.name" :value="item.id" :key="item.name"></el-option>
+                            <template v-for="child in item.children">
+                                <el-option :label="child.name" :value="child.id" :key="child.name">
+                                    <span>{{ "- " + child.name }}</span>
+                                </el-option>
+                            </template>
+                        </template>
+                    </el-select>
+                    <el-alert title="未选择上级菜单，则默认为添加一级菜单" :closable="false" type="info" style="line-height: 12px;"></el-alert>
+                </el-form-item>
+
+                <el-form-item label="菜单名称" prop="name" label-width="100px">
+                    <el-input v-model="editForm.name" autocomplete="off"></el-input>
+                </el-form-item>
+
+                <el-form-item label="权限编码" prop="perms" label-width="100px">
+                    <el-input v-model="editForm.perms" autocomplete="off"></el-input>
+                </el-form-item>
+
+                <el-form-item label="图标" prop="icon" label-width="100px">
+                    <el-input v-model="editForm.icon" autocomplete="off"></el-input>
+                </el-form-item>
+
+                <el-form-item label="菜单URL" prop="path" label-width="100px">
+                    <el-input v-model="editForm.path" autocomplete="off"></el-input>
+                </el-form-item>
+
+                <el-form-item label="菜单组件" prop="component" label-width="100px">
+                    <el-input v-model="editForm.component" autocomplete="off"></el-input>
+                </el-form-item>
+
+                <el-form-item label="类型" prop="type" label-width="100px">
+                    <el-radio-group v-model="editForm.type">
+                        <el-radio v-for="item in menuType" :key="item.value" :label="item.value" :value="item.value">{{item.name}}</el-radio>
+                    </el-radio-group>
+                </el-form-item>
+
+                <el-form-item label="状态" prop="statu" label-width="100px">
+                    <el-radio-group v-model="editForm.statu">
+                        <el-radio v-for="item in menuStatu" :key="item.value" :label="item.value" :value="item.value">{{item.name}}</el-radio>
+                    </el-radio-group>
+                </el-form-item>
+
+                <el-form-item label="排序号" prop="orderNum" label-width="100px">
+                    <el-input-number v-model="editForm.orderNum" :min="1" label="排序号">1</el-input-number>
+                </el-form-item>
+
+                <el-form-item>
+                    <el-button type="primary" @click="submitForm('editForm')">提交</el-button>
+                    <el-button @click="resetForm('editForm')">重置</el-button>
+                </el-form-item>
+            </el-form>
+
+        </el-dialog>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: "MenuEdit",
+        inject:['reload'],
+        data() {
+            return {
+                dialogVisible: false,               //是否显示对话框
+                editForm: {},                       //编辑内容
+                editFormRules: {                    //检验规则
+                    name: [
+                        {required: true, message: '请输入名称', trigger: 'blur'}
+                    ],
+                    perms: [
+                        {required: true, message: '请输入权限编码', trigger: 'blur'}
+                    ],
+                    type: [
+                        {required: true, message: '请选择状态', trigger: 'blur'}
+                    ],
+                    orderNum: [
+                        {required: true, message: '请填入排序号', trigger: 'blur'}
+                    ],
+                    statu: [
+                        {required: true, message: '请选择状态', trigger: 'blur'}
+                    ]
+                },
+                treeData:[],                        //树形菜单数据
+                menuType: [],                      //菜单类型
+                menuStatu: []                      //菜单状态
+            }
+        },
+        created(){
+            this.getTreeMenu()
+            this.getMenuType()
+            this.getMenuStatu()
+        },
+        methods: {
+            // 窗口初始化方法
+            init(id) {
+                this.$nextTick(() => {
+                    if(id){
+                        this.$axios.get('/sys/menu/info/' + id).then(res => {
+                            this.editForm = res.data.data;
+                            this.editForm.statu = res.data.data.statu.toString();
+                            this.editForm.type = res.data.data.type.toString();
+                            if(this.editForm.parentId === 0){
+                                this.editForm.parentId = "";
+                            }
+                        })
+                    }
+                    this.dialogVisible = true;
+                    this.title = "编辑角色";
+                });
+            },
+            //获取树形菜单
+            getTreeMenu(){
+                this.$axios.get('sys/menu/list').then(res =>{
+                    this.treeData = res.data.data;
+                })
+            },
+            //获取菜单类型
+            getMenuType(){
+                this.$axios.get('sys/dict/getListByCode',{params:{code:"MENU_TYPE"}}).then(res =>{
+                    this.menuType = res.data.data;
+                })
+            },
+            //获取菜单状态
+            getMenuStatu(){
+                this.$axios.get('sys/dict/getListByCode',{params:{code:"MENU_STATU"}}).then(res =>{
+                    this.menuStatu = res.data.data;
+                })
+            },
+            //重置
+            resetForm(formName) {
+                this.$refs[formName].resetFields();
+                this.editForm = {}
+            },
+            //关闭对话框
+            handleClose() {
+                this.resetForm('editForm')
+                this.dialogVisible = false
+            },
+            //提交
+            submitForm(formName) {
+                this.$refs[formName].validate((valid) => {
+                    if (valid) {
+                        this.$axios.post('/sys/menu/' + (this.editForm.id?'update' : 'save'), this.editForm).then(res => {
+                            console.log(res)
+                            this.$message({
+                                showClose: true,
+                                message: '提交成功',
+                                type: 'success',
+                                onClose:() => {
+                                    this.reload()
+                                }
+                            });
+                            this.resetForm('editForm')
+                            this.dialogVisible = false
+                        })
+                    } else {
+                        console.log('error submit!!');
+                        return false;
+                    }
+                });
+            },
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
